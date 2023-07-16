1e1451cbc3ec6129b3f0a9d703fa33338224c03d
Altino Leandro Rodiguês <136701250+AltinoLeandroRodrigues@users.noreply.github.com>
2023-07-16 13:56:45 +0100

    css

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
  
-function clicar(){
+function ver(){
     let visualizar = document.getElementById('ver');
     let password = document.getElementById('senha');
+    let n_visualizar = document.getElementById('oculto');
      
 
    let input_type = password.getAttribute("type")
     
     if (input_type == "password") {
         password.setAttribute("type","text");
-        visualizar.style. filter=" grayscale(0%)"; 
-        
+        visualizar.style. display="none"; 
+        n_visualizar.style.display="flex";
+         
+
     } else {
         password.setAttribute("type","password");
-        visualizar.style. filter=" grayscale(100%)";
+        visualizar.style. display="flex"; 
+        n_visualizar.style.display="none";    
     }
 }
