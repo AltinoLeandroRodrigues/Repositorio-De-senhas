be14c49a8a3c648b5495402913a6c8add3c82fb1
Altino Leandro Rodiguês <136701250+AltinoLeandroRodrigues@users.noreply.github.com>
2023-07-18 11:36:11 +0100

    Home

---
+++ Modal.js
@@ -0,0 +1,12 @@
+function Login(){
+    let Link=document.getElementById('Login');
+    let Formulario =document.getElementById('forma');
+    Formulario.style.display="flex";
+}
+function fechar(){
+    let fechar=document.getElementById('fechar');
+    let Formulario =document.getElementById('forma');
+    Formulario.style.display="none";
+     
+}
+ 
